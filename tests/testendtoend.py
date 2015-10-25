ac9f4215cfbdc73eec84f29edbd55163b7d337a5
Cameron Lee <cam.w.lee@gmail.com>
2015-10-24 23:18:30 -0600

    Unit tests for direct messages.

--- tests/testendtoend.py
+++ tests/testendtoend.py
@@ -25,6 +25,9 @@ class EndToEndTestCase(BaseTestCase):
     def setMessageReceived(self, agent, message):
         self.message = message
 
+    def setDirectMessageReceived(self, agent, num_id, message):
+        self.message = message
+
     def testMessageMatches(self):
         """
         Tests that the message is received by client1 when it matches
@@ -47,6 +50,29 @@ class EndToEndTestCase(BaseTestCase):
         self.waitForNetwork()
 
         self.assertEqual(self.message, None)
+
+    def testDirectMessage(self):
+        """
+        Tests that direct messages can be sent.
+        """
+        self.client1.bind_direct_msg(self.setDirectMessageReceived)
+        self.waitForNetwork()
+        self.client2.send_direct_message("client1", 1, "secret message")
+        self.waitForNetwork()
+
+        self.assertEqual(self.message, "secret message")
+
+    def testListMessage(self):
+        """
+        Tests that lists can be sent as direct messages (an extension 
+        of the ivy protocol implemented on Python only).
+        """
+        self.client1.bind_direct_msg(self.setDirectMessageReceived)
+        self.waitForNetwork()
+        self.client2.send_direct_message("client1", 1, ["list", "message"])
+        self.waitForNetwork()
+
+        self.assertEqual(self.message, ["list", "message"])
 
     def tearDown(self):
         self.client1.stop()
